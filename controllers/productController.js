6bbb3c089ae96969e45fca6e94a37f7073e37776
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-07-12 21:37:23 +0530

    filter changed

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,10 +4,34 @@ exports.getProductsByFilter = (req, res, next) => {
   const category = req.body.category;
   const price = req.body.price;
   const sort = req.body.sort;
-  const page= +req.query.page || 1;
-  console.log(category,price,sort,page)
+  const page= +req.body.page || 1;
+  const perPage=10;//Number of products per page
+  console.log(category+'hiiiii',price,sort,page)
 
-  Product.aggregate([
+  //Prepare the filter options based on category and price
+  const filterOptions={};
+  if(category!="all"){
+    filterOptions["category.name"]=category;
+  }
+  if(price){
+    filterOptions.price={$gte:parseInt(price)};
+    console.log(filterOptions);
+  }
+
+  //Prepare the sort option based on sort parameter
+  const sortOption={};
+  switch(sort){
+    case 'asc':
+      sortOption.price=1;
+      break;
+    case 'desc':
+      sortOption.price=-1;
+      break;
+    default:
+      //If needed handle it properly
+      break;
+  }
+  const  aggregationStages=[
     {
       $lookup:{
         from:'categories',
@@ -17,13 +41,34 @@ exports.getProductsByFilter = (req, res, next) => {
       }
     },
     {
-      $match:{
-        'category.name':category
-      }
+      $match:filterOptions,
+        // 'category.name':category 
+    },
+  ]
+  if(sortOption.price||(price && !category)){
+    aggregationStages.push({
+      $sort:sortOption,
+    });
+  }
+  aggregationStages.push(
+    {
+      $skip:(page-1)*perPage,
+    },
+    {
+      $limit:perPage,
     }
-  ])
+  );
+  Product.aggregate(aggregationStages)
   .then((products)=>{
-    return res.json({prods:products});
+     // Calculate the total number of filtered products
+    const totalProducts=products.length;
+     // Perform pagination on the filtered products
+     const paginatedProducts=products.slice((page-1)*perPage,page*perPage);
+    return res.json({
+      prods:products,
+      totalProducts,
+      filterProducts:paginatedProducts,
+    });
   })
     .catch((err) => {
       console.log(err);
