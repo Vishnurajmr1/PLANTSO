fb744909c779eb2b2cb9717b93357c29645b7ec4
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-07-15 11:46:55 +0530

    some changes added

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.use(errorController.get404);
 app.use((error, req, res, next) => {
     // res.status(error.httpStatusCode).render(...);
     // res.redirect('/500');
-    res.status(500).render("500", {
+    res.status(500).render("error", {
         pageTitle: "Error!",
         path: "/500",
         isAuthenticated: req.session.isLoggedIn
